2641a029a0952dda8b0e33a86291d80c90d78abb
onureredo <onureredo@yahoo.com>
2024-04-29 08:14:08 +0200

    swanson

---
+++ Week 06/03 Workshops/Ron Swanson/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>
+        <a href='/instructions.html'> instructions </a>
+      </h1>
+      <img
+        src='https://media.giphy.com/media/tSVnUxoWoHC/giphy.gif'
+        alt='ron'
+      />
+      <p>QUOTE</p>
+    </div>
+  );
+}
+
+export default App;
